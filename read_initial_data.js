89474ac6472580a542ea202814ae3eab46dd2df7
amirali <amirali.mirian@gmail.com>
2020-04-01 06:10:19 +0430

    on app start, it will make the input file.

--- read_initial_data.js
+++ read_initial_data.js
@@ -2,8 +2,14 @@ let fs = require('fs');
 
 module.exports = {
     read_data: function () {
-        let obj = JSON.parse(fs.readFileSync('inputfile.json', 'utf8'));
-        console.log('initial data has been read from the file.');
-        return obj;
+        try {
+            let obj = JSON.parse(fs.readFileSync('inputfile.json', 'utf8'));
+            console.log('initial data has been read from the file.');
+            return obj;
+        } catch (e) {
+            fs.copyFileSync('empty_feature_collection.json', 'inputfile.json');
+            console.log('input file created.');
+            return JSON.parse(fs.readFileSync('empty_feature_collection.json', 'utf8'));
+        }
     },
 };
