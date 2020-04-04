ffa6975fd7046f6ad9c6da530f79875c2e1b12bb
amirali <amirali.mirian@gmail.com>
2020-04-04 22:42:57 +0430

    added winston logger.

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const express = require('express');
 const read_initial_data = require('./read_initial_data');
 const get_request_helper = require('./get_request_helper');
 const put_request_helper = require('./put_reqeust_helper');
+const morgan = require('morgan');
+let winston = require('./config/winston');
 
 const app = express();
+app.use(morgan('combined', {stream: winston.stream}));
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
 let geo_data = read_initial_data.read_data();
 console.log('initial data has been loaded to memory.');
+
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // add this line to include winston logging
+    winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 app.get('/gis/testpoint', function (request, response) {
     let point = [parseFloat(request.query.lat), parseFloat(request.query.long)];
